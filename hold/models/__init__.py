b57ba8bb2904a5b23ae22320d9515702a2fc75a8
Ibukun16 <ib.agunbiade18@gmail.com>
2024-05-20 05:06:42 -0700

    Test for pycodestyle compliant

--- hold/models/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-This __init__ module contains method for the models directory
-"""
-from models.engine.file_storage import FileStorage
-
-
-storage = FileStorage()
-storage.reload()
